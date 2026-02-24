003cb2bd8eafa93812928d0952643203b0c69a76
adrian <adrianreysanchez1@gmail.com>
2026-02-24 22:42:49 +0100

    Base para el mejor proyecto echo en peru

---
+++ src/main/java/com/example/apiPeliculasTFG/ApiPeliculasTfgApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiPeliculasTFG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPeliculasTfgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPeliculasTfgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apiPeliculasTFG/ApiPeliculasTfgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiPeliculasTFG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPeliculasTfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
