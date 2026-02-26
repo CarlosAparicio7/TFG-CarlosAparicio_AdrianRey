58ed56dcc304c0fe59fed4c8c9631b1f0c3dda79
CarlosAparicio7 <ninjacar04@gmail.com>
2026-02-26 18:50:19 +0100

    Ahora si está entera la clase Usuarios que no lo había guardado

--- src/main/java/com/example/apiPeliculasTFG/entity/Usuarios.java
+++ src/main/java/com/example/apiPeliculasTFG/entity/Usuarios.java
@@ -12,5 +12,60 @@ public class Usuarios {
 	private String avatarIcon;
 	private String rol;
 	
+	public Usuarios() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Usuarios(String id, String nombre, String apellido, String avatarIcon, String rol) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.avatarIcon = avatarIcon;
+		this.rol = rol;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getAvatarIcon() {
+		return avatarIcon;
+	}
+
+	public void setAvatarIcon(String avatarIcon) {
+		this.avatarIcon = avatarIcon;
+	}
+
+	public String getRol() {
+		return rol;
+	}
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+	
+	
 	
 }
