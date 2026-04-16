9a5ee21a85185df3568eb42df55af33ca5b93a45
adrian <adrianreysanchez1@gmail.com>
2026-04-16 18:54:54 +0100

    Cambio variable id a string

--- src/main/java/com/example/apiPeliculasTFG/service/UsuariosService.java
+++ src/main/java/com/example/apiPeliculasTFG/service/UsuariosService.java
@@ -12,45 +12,45 @@ import com.example.apiPeliculasTFG.repository.UsuariosRepository;
 @Service
 public class UsuariosService {
 
-    @Autowired
-    private UsuariosRepository usuariosRepository;
+    @Autowired
+    private UsuariosRepository usuariosRepository;
 
-    public List<Usuarios> obtenerTodos() {
-        return usuariosRepository.findAll();
-    }
-    
-    public Usuarios crearUsuario(Usuarios usuario) {
-        return usuariosRepository.save(usuario);
-    }
-    
-    public Usuarios actualizarUsuario(Long id, Usuarios usuarioActualizado) {
-        return usuariosRepository.findById(id).map(usuarioExistente -> {
-            usuarioExistente.setNombre(usuarioActualizado.getNombre());
-            usuarioExistente.setApellido(usuarioActualizado.getApellido());
-            usuarioExistente.setAvatarIcon(usuarioActualizado.getAvatarIcon());
-            usuarioExistente.setRol(usuarioActualizado.getRol());
-            usuarioExistente.setEmail(usuarioActualizado.getEmail());
-            usuarioExistente.setPassword(usuarioActualizado.getPassword());
-            
-            return usuariosRepository.save(usuarioExistente);
-        }).orElseThrow(() -> new RuntimeException("Usuario no encontrado con ID: " + id));
-    }
-    
-    public void eliminarUsuario(Long id) {
-        if (usuariosRepository.existsById(id)) {
-            usuariosRepository.deleteById(id);
-        } else {
-            throw new RuntimeException("Usuario no encontrado con ID: " + id);
-        }
-    }
+    public List<Usuarios> obtenerTodos() {
+        return usuariosRepository.findAll();
+    }
+    
+    public Usuarios crearUsuario(Usuarios usuario) {
+        return usuariosRepository.save(usuario);
+    }
+    
+    public Usuarios actualizarUsuario(String id, Usuarios usuarioActualizado) {
+        return usuariosRepository.findById(id).map(usuarioExistente -> {
+            usuarioExistente.setNombre(usuarioActualizado.getNombre());
+            usuarioExistente.setApellido(usuarioActualizado.getApellido());
+            usuarioExistente.setAvatarIcon(usuarioActualizado.getAvatarIcon());
+            usuarioExistente.setRol(usuarioActualizado.getRol());
+            usuarioExistente.setEmail(usuarioActualizado.getEmail());
+            usuarioExistente.setPassword(usuarioActualizado.getPassword());
+            
+            return usuariosRepository.save(usuarioExistente);
+        }).orElseThrow(() -> new RuntimeException("Usuario no encontrado con ID: " + id));
+    }
+    
+    public void eliminarUsuario(String id) {
+        if (usuariosRepository.existsById(id)) {
+            usuariosRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Usuario no encontrado con ID: " + id);
+        }
+    }
 
-    public Usuarios login(String email, String password) {
-        Optional<Usuarios> usuario = usuariosRepository.findByEmail(email);
-        
-        if (usuario.isPresent() && usuario.get().getPassword().equals(password)) {
-            return usuario.get();
-        }
-        
-        return null;
-    }
+    public Usuarios login(String email, String password) {
+        Optional<Usuarios> usuario = usuariosRepository.findByEmail(email);
+        
+        if (usuario.isPresent() && usuario.get().getPassword().equals(password)) {
+            return usuario.get();
+        }
+        
+        return null;
+    }
 }
